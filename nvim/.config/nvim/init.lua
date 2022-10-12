c7edc45f1aa1eb4f54d78bbfbb5fa59ecc35d535
Giuseppe Carlino <g.carlino@simularia.it>
2022-10-12 17:39:31 +0200

    Added LSP keymaps [nvim]

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -504,47 +504,47 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Goto next diagnost
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist,
     { desc = "Add buffer diagnostic to the location list" })
 
--- vim.api.nvim_create_autocmd('LspAttach', {
---     callback = function(args)
---
--- --     -- Trigger completion with <c-x><c-o>
---         vim.api.nvim_buf_set_option(args.buf, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
---
---         local bufopts = { buffer = args.buf, noremap = true, silent = true }
---
---         vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
---         vim.keymap.set('n', '<leader>F', vim.lsp.buf.format, bufopts)
---         vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
---         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
---         vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
---
---         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
---         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
---         -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
---         vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition,
---             { desc = "Jumps to the definition of the type of the symbol under the cursor." })
---         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename,
---             { desc = "Renames all references to the symbol under the cursor." })
---         vim.keymap.set('n', '<leader>so',
---             function()
---                 require('telescope.builtin').lsp_document_symbols()
---             end,
---             { desc = "Lists LSP symbols in the current buffer" })
---
---         -- additional capabilities
---         local capabilities = vim.lsp.protocol.make_client_capabilities()
---
---         -- nvim-cmp supports additional completion capabilities
---         capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
---
---         -- -- tell the sever the capability of foldingRange
---         -- capabilities.textDocument.foldingRange = {
---         --     dynamicRegistration = false,
---         --     lineFoldingOnly = true
---         -- }
---
---     end
--- })
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+
+--     -- Trigger completion with <c-x><c-o>
+        vim.api.nvim_buf_set_option(args.buf, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+        local bufopts = { buffer = args.buf, noremap = true, silent = true }
+
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+        vim.keymap.set('n', '<leader>F', vim.lsp.buf.format, bufopts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
+
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+        -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition,
+            { desc = "Jumps to the definition of the type of the symbol under the cursor." })
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename,
+            { desc = "Renames all references to the symbol under the cursor." })
+        vim.keymap.set('n', '<leader>so',
+            function()
+                require('telescope.builtin').lsp_document_symbols()
+            end,
+            { desc = "Lists LSP symbols in the current buffer" })
+
+        -- additional capabilities
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+        -- nvim-cmp supports additional completion capabilities
+        capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+        -- -- tell the sever the capability of foldingRange
+        -- capabilities.textDocument.foldingRange = {
+        --     dynamicRegistration = false,
+        --     lineFoldingOnly = true
+        -- }
+
+    end
+})
 
 -- Use an on_attach function to only map the following keys after the language server attaches to the current buffer
 -- local on_attach = function(_, bufnr)
