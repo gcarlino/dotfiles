67290186164935ec5df97aafa23309e5a5419394
Giuseppe Carlino <g.carlino@simularia.it>
2022-10-15 16:20:11 +0200

    Moved ftplugin to after/ [nvim]

---
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -0,0 +1,1 @@
+require('./clangd.lua')
---
+++ nvim/.config/nvim/after/ftplugin/clangd.lua
@@ -0,0 +1,9 @@
+vim.lsp.start({
+    name = 'clangd',
+    cmd = { 'clangd' },
+    filetypes = { 'c', 'cpp' },
+    root_dir = vim.fs.dirname(
+        vim.fs.find(
+            {'.git', '.clangd' },
+            { upward = true })[1]),
+})
