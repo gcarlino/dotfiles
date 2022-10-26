1ae06d7679d2c793452576931b8cb03d393b5599
Giuseppe Carlino <g.carlino@simularia.it>
2022-10-26 09:39:40 +0200

    Folding setup [nvim]

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -730,6 +730,7 @@ vim.o.foldcolumn = '1'
 vim.o.foldlevel = 99 -- feel free to decrease the value
 vim.o.foldlevelstart = -1
 vim.o.foldenable = true
+vim.o.foldnestmax = 1
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
