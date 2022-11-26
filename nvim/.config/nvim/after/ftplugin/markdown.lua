1098db0f310bc288cadfd2d040fbe1eea820f6cd
Giuseppe Carlino <g.carlino@simularia.it>
2022-11-26 19:14:21 +0100

    Enable marksman [nvim]

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,10 +1,11 @@
 vim.opt.wrap = true
 
--- -- require('lsp.marksman')
--- vim.lsp.start({
---     name = 'marksman',
---     filetypes = { 'markdown' },
---     cmd = { 'marksman', 'server' },
---     root_dir = vim.fs.dirname(vim.fs.find({'.marksman.toml'}, { upward = true })[1]),
---     settings = {},
--- })
+-- require('lsp.marksman')
+vim.lsp.start({
+    name = 'marksman',
+    filetypes = { 'markdown' },
+    cmd = { 'marksman', 'server' },
+    root_dir = vim.fs.dirname(vim.fs.find({'.marksman.toml'}, { upward = true })[1]),
+    settings = {
+    },
+})
