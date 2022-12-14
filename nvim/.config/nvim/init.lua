08452ad6267708a2e654e30779423889f02fce17
Giuseppe Carlino <g.carlino@simularia.it>
2022-12-14 23:36:22 +0100

    Reorder init.lua [nvim]

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,26 +1,23 @@
 require("beps.options")
+require("beps.disable_builtin")
+require("beps.keymaps")
 
--- Mac spefic configuration
 if vim.fn.has("mac") == 1 then
     require("beps.mac")
 end
-
-require("beps.keymaps")
-require("beps.disable_builtin")
 
 -- lewis6991/impatient.nvim 
 require('impatient')
 
 require("plugins")
-
-require('beps.colorscheme')
 require('beps.plugins.treesitter')
 require('beps.plugins.telescope')
 require('beps.plugins.dap')
-require('beps.lsp')
 require('beps.plugins.nvim-cmp')
 require('beps.plugins.gitsigns')
 require('beps.plugins.diffview')
 require("beps.plugins.lualine")
 require('beps.plugins.nvim-ufo')
 
+require('beps.lsp')
+require('beps.colorscheme')
