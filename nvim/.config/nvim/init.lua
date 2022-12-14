7c8618a24ef06024c8da5ef88270ea2bbf0514c1
Giuseppe Carlino <g.carlino@simularia.it>
2022-12-14 23:22:36 +0100

    Set terminal keymaps [nvim]

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -28,25 +28,3 @@ require('beps.plugins.diffview')
 require("beps.plugins.lualine")
 require('beps.plugins.nvim-ufo')
 
-
--- Mappings to make moving in and out of a terminal easier once toggled,
--- whilst still keeping it open
-function _G.set_terminal_keymaps()
-    local opts = { buffer = 0 }
-    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-    vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-end
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-
--- -- jalvesazq/Nvim-R
--- vim.cmd([[
---     let R_assign_map = '<M-->'
---     let rout_follow_colorscheme = 1
---     let r_syntax_folding = 1
---     set nofoldenable
---     autocmd VimLeave * if exists("g:SendCmdToR") && string(g:SendCmdToR) != "function('SendCmdToR_fake')" | call RQuit("nosave") | endif
--- ]])
--- nvim/.config/nvim/lua/beps/keymaps.lua
+++ nvim/.config/nvim/lua/beps/keymaps.lua
@@ -47,3 +47,18 @@ vim.keymap.set('x', '<leader>/', '<ESC><CMD>lua require("Comment.api").toggle.li
 
 -- Allow gf to open non-existent files
 vim.keymap.set('', 'gf', ':edit <cfile><CR>')
+
+
+-- Mappings to make moving in and out of a terminal easier once toggled,
+-- whilst still keeping it open
+function _G.set_terminal_keymaps()
+    local opts = { buffer = 0 }
+    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+    vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+end
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
