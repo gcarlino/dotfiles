7f529b6e770ada5b6df35a4c306d450abaf9315d
Giuseppe Carlino <g.carlino@simularia.it>
2022-12-17 12:26:15 +0100

    Move around change background color function [nvim]

--- nvim/.config/nvim/lua/beps/keymaps.lua
+++ nvim/.config/nvim/lua/beps/keymaps.lua
@@ -58,3 +58,18 @@ function _G.set_terminal_keymaps()
 end
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
+-- Toggle backgournd color
+vim.api.nvim_set_keymap("n", "<leader>b", "", {
+    noremap = true,
+    callback = function()
+        local _background = vim.api.nvim_get_option("background")
+        if (_background == "light") then
+            vim.cmd('colorscheme nordfox')
+            vim.api.nvim_set_option('background', 'dark')
+        else
+            vim.cmd('colorscheme dayfox')
+            vim.api.nvim_set_option('background', 'light')
+        end
+    end,
+    desc = "Toggle background color"
+})
