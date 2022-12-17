803818ba49b053759329c209e16aeaca06488fee
Giuseppe Carlino <g.carlino@simularia.it>
2022-12-17 11:39:48 +0100

    Moved plugin configurations to after ... [nvim]
    ... as they are read automatically without need to require them.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,16 +7,7 @@ require("beps.keymaps")
 
 -- lewis6991/impatient.nvim 
 require('impatient')
---
-require("plugins")
-require('beps.plugins.treesitter')
-require('beps.plugins.telescope')
-require('beps.plugins.dap')
-require('beps.plugins.nvim-cmp')
-require('beps.plugins.gitsigns')
-require('beps.plugins.diffview')
-require("beps.plugins.lualine")
--- require('beps.plugins.nvim-ufo')
---
+
+require("beps.packer")
 require('beps.lsp')
 require('beps.colorscheme')
