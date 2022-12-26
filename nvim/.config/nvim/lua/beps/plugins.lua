fe15e5c6cb5f8da5bd149de3a9f556c5f8e8b6fc
Giuseppe Carlino <g.carlino@simularia.it>
2022-12-26 15:50:47 +0100

    Packer bootstrap [nvim]

--- nvim/.config/nvim/lua/beps/plugins.lua
+++ nvim/.config/nvim/lua/beps/plugins.lua
@@ -1,7 +1,20 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 -- lewis6991/impatient.nvim 
 require('impatient')
 
-require('packer').startup({ function(use)
+return require('packer').startup({ function(use)
     -- Package manager
     use 'wbthomason/packer.nvim'
 
@@ -231,5 +244,8 @@ require('packer').startup({ function(use)
             run = 'make install', })
     end
 
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end,
 })
