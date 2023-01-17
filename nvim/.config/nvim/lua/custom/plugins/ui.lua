7207b587b2405212781f0db9d656ffa67727a9d8
Giuseppe Carlino <g.carlino@simularia.it>
2023-01-18 00:24:42 +0100

    Fix indent-blankline loadin [nvim]

--- nvim/.config/nvim/lua/custom/plugins/ui.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui.lua
@@ -18,8 +18,8 @@ return {
     },
     {
         'lukas-reineke/indent-blankline.nvim',
-        -- event = "VeryLazy",
-        event = "BufReadPre",
+        event = "VeryLazy",
+        -- event = "BufReadPre",
         config = function()
             require('indent_blankline').setup()
         end,
