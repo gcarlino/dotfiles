04958f9801ab7aa1c1b5ad0407b3fac5e9c3d527
Giuseppe Carlino <g.carlino@simularia.it>
2023-01-24 13:03:13 +0100

    Remove unused colorscheme [nvim]

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,9 +1,5 @@
 return {
 
-    { 'https://github.com/sainnhe/edge', enable = false },
-
-    { 'navarasu/onedark.nvim', enable = false },
-
     {
         'EdenEast/nightfox.nvim',
         event = "VimEnter",
