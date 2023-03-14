cd70d768e9ad6bfeeeeb8cb8345fad1a2fe076b9
Giuseppe Carlino <g.carlino@simularia.it>
2023-03-14 13:41:18 +0100

    Move statusline config to ui setup [nvim]

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -2,44 +2,6 @@ return {
     { 'nvim-lua/plenary.nvim', lazy = true },
 
     {
-        'kevinhwang91/nvim-ufo',
-        event = "BufRead",
-        dependencies = {
-            { 'kevinhwang91/promise-async' },
-            {
-                'luukvbaal/statuscol.nvim',
-                config = function()
-                    local builtin = require("statuscol.builtin")
-                    require("statuscol").setup(
-                    {
-                        relculright = true,
-                        segments = {
-                            {text = {builtin.foldfunc}, click = "v:lua.ScFa"},
-                            {text = {"%s"}, click = "v:lua.ScSa"},
-                            {text = {builtin.lnumfunc, " "}, click = "v:lua.ScLa"}
-                        }
-                    }
-                    )
-                end
-            },
-        },
-        config = function ()
-            -- Fold options
-            vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-            vim.o.foldcolumn = "1" -- '0' is not bad
-            vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-            vim.o.foldlevelstart = 99
-            vim.o.foldenable = true
-
-            -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-            vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-            vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-
-            require("ufo").setup()
-        end
-    },
-
-    {
         'kyazdani42/nvim-tree.lua',
         keys = {
             {'<leader>n', ':NvimTreeToggle<CR>', desc = "Toggle nvim-tree" }
