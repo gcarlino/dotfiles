80c6dfd40e09107ea7936ec7d1c9ea94a8019ddb
Giuseppe Carlino <g.carlino@simularia.it>
2023-04-03 22:00:00 +0200

    LSP config [nvim]

--- nvim/.config/nvim/lua/beps/plugins/lsp.lua
+++ nvim/.config/nvim/lua/beps/plugins/lsp.lua
@@ -22,34 +22,33 @@ end
 
 lspconfig.pylsp.setup({})
 lspconfig.lua_ls.setup({
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
     },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-
 })
 lspconfig.texlab.setup({})
 lspconfig.clangd.setup({})
 lspconfig.cmake.setup({})
 lspconfig.fortls.setup({
-    filetypes = {'fortran', 'fortran77'},
+    filetypes = { 'fortran', 'fortran77' },
 })
 lspconfig.marksman.setup({})
 lspconfig.r_language_server.setup({})
@@ -59,33 +58,49 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
+local optsDesc = function(ev, mdesc)
+    local opts = { buffer = ev.buf, noremap = true, silent = true, desc = mdesc }
+    return opts
+end
+
 vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-
     callback = function(ev)
         -- Enable completion triggered by <c-x><c-o>
         vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
         -- Buffer local mappings.
         -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local opts = { buffer = ev.buf }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<space>f', function()
-            vim.lsp.buf.format { async = true }
-        end, opts)
+        -- local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, optsDesc(ev, 'LSP goto declaration'))
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, optsDesc(ev, 'LSP goto definition'))
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, optsDesc(ev, 'LSP show hover information.'))
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, optsDesc(ev, 'Show implementation.'))
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, optsDesc(ev, 'LSP signature help'))
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, optsDesc(ev, 'LSP add workspace folder'))
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, optsDesc(ev, 'LSP remove workspace folder'))
+        vim.keymap.set('n', '<space>wl',
+        function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
+        optsDesc(ev, 'LSP list workspace folders'))
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, optsDesc(ev, 'LSP type definition.'))
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, optsDesc(ev, 'LSP buffer rename'))
+        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, optsDesc(ev, 'LSP buffer code action.'))
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, optsDesc(ev, 'LSP references.'))
+        vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end,
+        optsDesc(ev, 'LSP buffer format'))
+
+        -- Telescope LSP commands
+        vim.keymap.set('n', '<leader>lt', function() require('telescope.builtin').diagnostics() end,
+            optsDesc(ev, 'LSP document diagnostics ')
+        )
+        vim.keymap.set('n', '<leader>lr',
+            function() require('telescope.builtin').lsp_references() end,
+            { desc = 'LSP refs for word under cursor ' }
+        )
+        vim.keymap.set('n', '<leader>ls',
+            function() require('telescope.builtin').lsp_document_symbols() end,
+            { desc = 'LSP document symbols ' }
+        )
 
         -- additional capabilities
         local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -93,76 +108,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
         -- nvim-cmp supports additional completion capabilities
         -- capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
         capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
     end,
     -- callback = function(args)
-    --
-    --     local bufdesc = function (mdesc)
-    --         local bufdesc = { buffer = args.buf, noremap = true, silent = true, desc = mdesc }
-    --         return bufdesc
-    --     end
-    --     local bufopts = { buffer = args.buf, noremap = true, silent = true, desc = "LSP" }
-    --
-    --     -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    --     vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<cr>',
-    --         bufopts
-    --     )
-    --     vim.keymap.set('n', '<leader>lf', '<cmd>Lspsaga lsp_finder<cr>',
-    --         bufdesc("LSP finder")
-    --     )
-    --     vim.keymap.set('n', '<leader>lF', vim.lsp.buf.format,
-    --         bufdesc("LSP buffer format")
-    --     )
-    --     vim.keymap.set('n', 'gr', vim.lsp.buf.references,
-    --         bufdesc("LSP references")
-    --     )
-    --     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation,
-    --         bufdesc("LSP implementation")
-    --     )
-    --     -- vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
-    --     vim.keymap.set('n', 'ga', '<cmd>Lspsaga code_action<cr>',
-    --         bufdesc("LSP code action")
-    --     )
-    --     vim.keymap.set('n', '<leader>lo', '<cmd>Lspsaga outline<cr>',
-    --         bufdesc("LSP outline")
-    --     )
-    --     -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    --     vim.keymap.set('n', 'gd', '<cmd>Lspsaga peek_definition<cr>',
-    --         bufdesc("LSP peek definition")
-    --     )
-    --     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration,
-    --         bufdesc("LSP declaration")
-    --     )
-    --     -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    --     vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition,
-    --         bufdesc("LSP type definition")
-    --     )
-    --     -- vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename,
-    --     vim.keymap.set('n', '<leader>rn', '<cmd>Lspsaga rename<cr>',
-    --         bufdesc("LSP rename")
-    --     )
-    --     vim.keymap.set('n', '<leader>ld', '<cmd>Lspsaga show_line_diagnostics<cr>',
-    --         bufdesc("LSP show line diagnostic")
-    --     )
-    --     vim.keymap.set('n', '[d', '<cmd>Lspsaga diagnostic_jump_prev<cr>',
-    --         bufdesc("Goto prev diagnostic")
-    --     )
-    --     vim.keymap.set('n', ']d', '<cmd>Lspsaga diagnostic_jump_next<cr>',
-    --         bufdesc("Goto next diagnostic")
-    --     )
-    --
-    --     -- -- Telescope LSP commands
-    --     vim.keymap.set('n', '<leader>lt', function() require('telescope.builtin').diagnostics() end,
-    --         bufdesc("LSP document diagnostics ")
-    --     )
-    --     vim.keymap.set('n', '<leader>lr',
-    --         function() require('telescope.builtin').lsp_references() end,
-    --         { desc = "LSP refs for word under cursor " }
-    --     )
-    --     vim.keymap.set('n', '<leader>ls',
-    --         function() require('telescope.builtin').lsp_document_symbols() end,
-    --         { desc = "LSP document symbols " }
-    --     )
     --
     --     -- additional capabilities
     --     local capabilities = vim.lsp.protocol.make_client_capabilities()
