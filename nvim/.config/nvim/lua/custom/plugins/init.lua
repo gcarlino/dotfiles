7cbcac3b8e157800ba75b0947b3d2d6da7342165
Giuseppe Carlino <g.carlino@simularia.it>
2023-05-17 18:22:08 +0200

    Clean toggleterm config [nvim]

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -85,16 +85,9 @@ return {
     {
         'akinsho/toggleterm.nvim',
         event = 'VeryLazy',
-        config = function()
-            require('toggleterm').setup {
-                open_mapping = [[<c-q>]],
-            }
-            -- disable spell checking in terminal
-            vim.api.nvim_create_autocmd("TermOpen", {
-                pattern = "term://*",
-                callback = function () vim.o.spell = false end
-            })
-        end
+        opts = {
+            open_mapping = [[<c-q>]],
+        }
     },
 
     {
