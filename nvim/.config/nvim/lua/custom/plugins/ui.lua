6bb1a732bfe33a01b50365d8657b971a5c82e06b
Giuseppe Carlino <g.carlino@simularia.it>
2023-09-26 00:43:30 +0200

    Reorder status column [nvim].

--- nvim/.config/nvim/lua/custom/plugins/ui.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui.lua
@@ -29,9 +29,6 @@ return {
                 setopt = true,
                 relculright = true,
                 segments = {
-                    { text = { builtin.lnumfunc },
-                        auto = true,
-                        click = "v:lua.ScLa", },
                     {
                         sign = {
                             name = { ".*" },
@@ -44,63 +41,21 @@ return {
                     },
                     {
                         sign = {
-                            name = { "Diagnostic" },
-                            maxwidth = 2,
+                            name = { "DiagnosticSign" },
+                            maxwidth = 1,
                             colwidth = 1,
                             auto = false
                         },
                         click = "v:lua.ScSa"
                     },
+                    { text = { builtin.lnumfunc },
+                        auto = true,
+                        click = "v:lua.ScLa", },
                     { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
                 }
             })
         end,
     },
-
-    -- {
-    --     'kevinhwang91/nvim-ufo',
-    --     event = "VeryLazy",
-    --     enabled = false,
-    --     dependencies = {
-    --         { 'kevinhwang91/promise-async' },
-    --         {
-    --             'luukvbaal/statuscol.nvim',
-    --             config = function()
-    --                 local builtin = require("statuscol.builtin")
-    --                 require("statuscol").setup({
-    --                     relculright = true,
-    --                     segments = {
-    --                         {text = {"%s"}, click = "v:lua.ScSa"},
-    --                         {text = {builtin.lnumfunc, " "}, click = "v:lua.ScLa"},
-    --                         {
-    --                             text = {builtin.foldfunc, " "},
-    --                             click = "v:lua.ScFa",
-    --                         }
-    --                     }
-    --                 })
-    --             end
-    --         },
-    --     },
-    --     config = function ()
-    --         -- Fold options
-    --         vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-    --         vim.o.foldcolumn = "1" -- '0' is not bad
-    --         vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-    --         vim.o.foldlevelstart = 99
-    --         vim.o.foldenable = true
-    --
-    --         -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-    --         vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-    --         vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-    --
-    --         -- Treesitter as the main provider
-    --         require("ufo").setup({
-    --             provider_selector = function (bufnr, filetype, buftype)
-    --                 return {'treesitter', 'indent'}
-    --             end
-    --         })
-    --     end
-    -- },
 
     {
         'lukas-reineke/indent-blankline.nvim',
@@ -120,11 +75,6 @@ return {
                 char = "│" ,
             })
         end
-    },
-
-    {
-        'nvim-tree/nvim-web-devicons',
-        lazy = true,
     },
 
     {
