38513da54787500b83e8ac98968d469eabe8ebfe
Giuseppe Carlino <g.carlino@simularia.it>
2023-10-23 22:50:16 +0200

    More compact statuscol [nvim]

--- nvim/.config/nvim/lua/custom/plugins/ui.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui.lua
@@ -31,35 +31,14 @@ return {
                 setopt = true,
                 relculright = true,
                 segments = {
+                    { text = { "%s" }, click = "v:lua.ScSa" },
                     {
-                        sign = {
-                            text = { ".*" },
-                            maxwidth = 1,
-                            colwidth = 1,
-                            auto = true,
-                            wrap = true
-                        },
-                        click = "v:lua.ScSa"
+                        text = { builtin.lnumfunc, " " },
+                        condition = { true, builtin.not_empty },
+                        click = "v:lua.ScLa",
                     },
-                    {
-                        sign = {
-                            name = { "DiagnosticSign" },
-                            maxwidth = 1,
-                            colwidth = 2,
-                            auto = true
-                        },
-                        click = "v:lua.ScSa"
-                    },
-                    {
-                        text = { builtin.lnumfunc },
-                        auto = true,
-                        click = "v:lua.ScLa"
-                    },
-                    {
-                        text = { builtin.foldfunc },
-                        click = "v:lua.ScFa"
-                    },
-                }
+                    { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+                },
             })
         end,
     },
