7f5f92f2b0605657d3892baea75c6943a3ddfa22
Giuseppe Carlino <g.carlino@simularia.it>
2024-03-10 12:07:41 +0100

    Update nvim-cmp key mappings [nvim].

--- nvim/.config/nvim/lua/beps/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/beps/plugins/nvim-cmp.lua
@@ -11,11 +11,11 @@ local cmp = require('cmp')
 local luasnip = require('luasnip')
 local lspkind = require('lspkind')
 
-local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
+-- local has_words_before = function()
+--   unpack = unpack or table.unpack
+--   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+--   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+-- end
 
 cmp.setup({
     -- completion = {
@@ -39,7 +39,7 @@ cmp.setup({
                 end
             },
         },
-        -- { name = "cmdline" },
+        { name = "cmdline" },
         {
             name = "latex_symbols",
             option = {
@@ -57,32 +57,28 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ["<C-u>"] = cmp.mapping.scroll_docs(-4),
         ["<C-d>"] = cmp.mapping.scroll_docs(4),
-        ["<C-e>"] = cmp.mapping.abort(),
+        ["<C-a>"] = cmp.mapping.abort(),
+
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+
         ["<C-Space>"] = cmp.mapping.complete({}),
-        ['<CR>'] = cmp.mapping.confirm({
-            behaviour = cmp.ConfirmBehavior.Replace,
-            select = false,
-        }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
+        ["<C-y>"] = cmp.mapping.confirm { select = true },
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+
+        -- <c-l> will move you to the right of each of the expansion locations.
+        -- <c-h> is similar, except moving you backwards.
+        ["<C-l>"] = cmp.mapping(function()
+            if luasnip.expand_or_locally_jumpable() then
                 luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
-            else
-                fallback()
             end
-        end, { 'i', 's' }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
+        end, { "i", "s" }),
+        ["<C-h>"] = cmp.mapping(function()
+            if luasnip.locally_jumpable(-1) then
                 luasnip.jump(-1)
-            else
-                fallback()
             end
-        end, { 'i', 's' }),
+        end, { "i", "s" }),
+
     }),
     -- nvim-cmp by defaults disables autocomplete for prompt buffers
     enabled = function()
