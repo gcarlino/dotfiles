349f8f0223a60f191107ccf43451a3837989f76e
Giuseppe Carlino <g.carlino@simularia.it>
2024-04-07 00:23:16 +0200

    fix: disable Comment.nvim [nvim]

--- nvim/.config/nvim/lua/custom/plugins/comment.lua
+++ nvim/.config/nvim/lua/custom/plugins/comment.lua
@@ -2,6 +2,7 @@ return {
     {
         'numToStr/Comment.nvim',
         event = "VeryLazy",
+        enabled = false,
         opts = {},
         -- config = function ()
         --     -- vim.keymap.set("n", "<D-/>", ":lua require('Comment.api').toggle.linewise.current(); vim.cmd('normal j')<CR>", {silent=true})
