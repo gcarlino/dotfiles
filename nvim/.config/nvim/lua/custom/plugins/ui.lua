cffc55d83aa593d8bfb4f15607d61fc347947137
Giuseppe Carlino <g.carlino@simularia.it>
2024-04-12 13:03:22 +0200

    Update which-key config [nvim].

--- nvim/.config/nvim/lua/custom/plugins/ui.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui.lua
@@ -7,20 +7,25 @@ return {
 
     {
         "folke/which-key.nvim",
-        event = "VeryLazy",
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 1000
-        end,
-        opts = {
-            window = {
-                border = "single",
-            },
-            disable = {
-                buftypes = {},
-                filetypes = { "TelescopePrompt" },
-            },
-        },
+        -- event = "VeryLazy",
+        event = "VimEnter",
+        -- init = function()
+        --     -- vim.o.timeout = true
+        --     -- vim.o.timeoutlen = 1000
+        -- end,
+        -- opts = {
+        --     window = {
+        --         border = "single",
+        --     },
+        --     disable = {
+        --         buftypes = {},
+        --         filetypes = {},
+        --     },
+        -- },
+        config = function ()
+            require('which-key').setup()
+        end
+
     },
 
     {
