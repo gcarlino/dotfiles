8b95e823cee729e0294c36cdf7134afa4d90316f
Giuseppe Carlino <g.carlino@simularia.it>
2024-05-24 23:42:43 +0200

    Remove unused colorschemes [nvim]

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -14,65 +14,6 @@ return {
     },
 
     {
-        'catppuccin/nvim',
-        name = 'catppuccin',
-        enabled = false,
-        opts = {
-            integrations = {
-                cmp = true,
-                gitsigns = true,
-                indent_blankline = { enabled = true },
-                markdown = true,
-                telescope = true,
-                treesitter = true,
-                treesitter_context = true,
-                which_key = true,
-                neotree = true,
-                mason = true,
-                native_lsp = {
-                    enabled = true,
-                    underlines = {
-                        errors = { "undercurl" },
-                        hints = { "undercurl" },
-                        warnings = { "undercurl" },
-                        information = { "undercurl" },
-                    }
-                }
-            },
-        },
-    },
-
-    {
-        'EdenEast/nightfox.nvim',
-        lazy = false,
-        priority = 1000,
-        enabled = false,
-        config = function()
-            require('nightfox').setup({
-                options = {
-                    terminal_colors = false,
-                    styles = {
-                        comments = 'italic',
-                    }
-                }
-            })
-            -- vim.cmd.colorscheme('nordfox')
-        end,
-    },
-
-    {
-        'sainnhe/edge',
-        lazy = false,
-        priority = 1000,
-        enabled = false,
-        config = function()
-            vim.g.edge_style = 'aura'
-            vim.g.edge_better_performance = 1
-            -- vim.cmd.colorscheme('edge')
-        end
-    },
-
-    {
         'folke/tokyonight.nvim',
         lazy = false,
         priority = 1000,
