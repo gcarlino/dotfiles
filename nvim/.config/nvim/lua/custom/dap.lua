81f1f3bab4d56e2daadf658e35366a841485829e
Giuseppe Carlino <g.carlino@simularia.it>
2024-07-12 17:23:36 +0200

    Update keymaps for DAP [nvim]

--- nvim/.config/nvim/lua/custom/dap.lua
+++ nvim/.config/nvim/lua/custom/dap.lua
@@ -16,8 +16,6 @@ function M.config_dap()
     }
     dap.adapters.cpp = dap.adapters.codelldb
     dap.adapters.c = dap.adapters.codelldb
-    dap.adapters.fortran = dap.adapters.codelldb
-    dap.adapters.fortran08 = dap.adapters.codelldb
 
     -- Load local configurations
     require('dap.ext.vscode').load_launchjs('./.nvim-dap/launch.json')
@@ -82,8 +80,10 @@ end
 -- key mappings --
 ------------------
 
+local set = vim.keymap.set
+
 -- Clear configurations, reload and continue
-vim.keymap.set("n", "<leader>dd",
+set("n", "<leader>dd",
     function()
         M.clear_configurations()
         M.config_dap()
@@ -93,66 +93,56 @@ vim.keymap.set("n", "<leader>dd",
     { desc = "DAP: debug load configurations with Telescope" }
 )
 
-vim.keymap.set('n', '<F5>', function() dap.continue() end, { desc = "DAP: debug continue" })
+set('n', '<leader>db', dap.toggle_breakpoint, { desc = "DAP: debug toggle [b]reakpoint" })
 
-vim.keymap.set('n', '<F4>', function() dap.run_last() end, { desc = "DAP: debug run last configuration" })
+set('n', '<F1>', dap.continue, { desc = "DAP: debug continue" })
+set('n', '<F2>', dap.step_over, { desc = "DAP: debug step over" })
+set('n', '<F3>', dap.step_into, { desc = "DAP: debug step into" })
+set('n', '<F4>', dap.step_out, { desc = "DAP: debug step out" })
 
-vim.keymap.set('n', '<F3>', function() dap.pause() end, { desc = "DAP: debug pause" })
+set('n', '<F5>', dap.run_last, { desc = "DAP: debug run last configuration" })
+set('n', '<F6>', dap.run_to_cursor, { desc = "DAP: debug run to cursor" })
+set('n', '<F11>', dap.pause, { desc = "DAP: debug pause" })
+set('n', '<F12>', dap.terminate, { desc = "DAP: debug terminate" })
 
-vim.keymap.set('n', '<F2>', function() dap.terminate() end, { desc = "DAP: debug terminate" })
+set("n", "<F8>", dap.up, { desc = "DAP: up in the stack." })
+set("n", "<F9>", dap.down, { desc = "DAP: down in the stack." })
 
-vim.keymap.set('n', '<F10>', function() dap.step_over() end, { desc = "DAP: debug step over" })
-
-vim.keymap.set('n', '<F11>', function() dap.step_into() end, { desc = "DAP: debug step into" })
-
-vim.keymap.set('n', '<F12>', function() dap.step_out() end, { desc = "DAP: debug step out" })
-
-vim.keymap.set('n', '<F8>', function() dap.run_to_cursor() end, { desc = "DAP: debug run to cursor" })
-
-vim.keymap.set('n', '<F9>', function() dap.toggle_breakpoint() end, { desc = "DAP: debug toggle breakpoint" })
-
-vim.keymap.set("n", "<F6>", function() dap.up() end, { desc = "DAP: up in the stack." })
-
-vim.keymap.set("n", "<F7>", function() dap.down() end, { desc = "DAP: downupup in the stack." })
-
-vim.keymap.set('n', '<leader>db', function() dap.set_breakpoint(vim.fn.input("Breakpoint condition: ")) end,
-    { desc = "DAP: debug set breakpoint condition" })
-
-vim.keymap.set('n', '<leader>dx',
+set('n', '<leader>dx',
     function()
         dap.disconnect()
         dap.close()
     end,
     { desc = "DAP: debug disconnect" })
 
-vim.keymap.set('n', '<leader>dt', function() dapui.toggle({}) end, { desc = "DAP: debug toggle dap-ui" })
+set('n', '<leader>dt', dapui.toggle, { desc = "DAP: debug toggle dap-ui" })
 
-vim.keymap.set('n', '<leader>dc', function() dapui.close({}) end, { desc = "DAP: debug close dap-ui" })
+set('n', '<leader>dc', dapui.close, { desc = "DAP: debug close dap-ui" })
 
 -- hover
-vim.keymap.set({ 'n', 'x' }, '<leader>di', function() dapui.eval() end,
+set({ 'n', 'x' }, '<leader>di', dapui.eval,
     { desc = "DAP: [i]nspect: eval the expression" })
 
-vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
+set({'n', 'v'}, '<Leader>dh', function()
     require('dap.ui.widgets').hover()
 end, { desc = "DAP: expression in a floating window" })
 
-vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
+set({'n', 'v'}, '<Leader>dp', function()
     require('dap.ui.widgets').preview()
 end, { desc = "DAP: expression in the preview window" })
 
-vim.keymap.set('n', '<Leader>df', function()
+set('n', '<Leader>df', function()
     local widgets = require('dap.ui.widgets')
     widgets.centered_float(widgets.frames)
 end, { desc = "DAP: print the stack frames" })
 
-vim.keymap.set('n', '<Leader>ds', function()
+set('n', '<Leader>ds', function()
     local widgets = require('dap.ui.widgets')
     widgets.centered_float(widgets.scopes)
     end, { desc = "DAP: print variables in the current scopes" })
 
 -- Reload launch json configuration
-vim.keymap.set('n', '<leader>dl', function() require("dap.ext.vscode").load_launchjs("./.nvim-dap/launch.json") end,
+set('n', '<leader>dl', function() require("dap.ext.vscode").load_launchjs("./.nvim-dap/launch.json") end,
     { desc = "DAP: load ./.nvim-dap/launch.json with debugger configuration" })
 
 -- Use nvim-dap events to open and close the windows automatically
