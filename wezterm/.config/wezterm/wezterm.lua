3c842e248aae142393e6bd2898cdc7cd461b0ad8
Giuseppe Carlino <g.carlino@simularia.it>
2024-07-12 17:27:53 +0200

    Major revision of configuration [nvim]
    General refactoring
    Keyboard shortcut to toggle colorschme
    New default window size

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,21 +1,49 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
+
+--
+-- CONFIGURATION
+--
+
 local config = wezterm.config_builder()
+
+-- config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 2000 }
+
+-- Disable automatic check for updated
+config.check_for_updates = false
 
 -- Window size at startup
 config.initial_cols = 120
-config.initial_rows = 28
+config.initial_rows = 32
 
--- Do not quit when all windows are closed
 config.quit_when_all_windows_are_closed = false
-
--- Do not display confirmation prompt
+config.adjust_window_size_when_changing_font_size = false
 config.window_close_confirmation = 'NeverPrompt'
+config.native_macos_fullscreen_mode = true
+config.scrollback_lines = 5000
+config.enable_scroll_bar = true
+config.font_size = 12
+config.font = wezterm.font {
+    family = 'MesloLGS NF',
+    weight = 'Regular'
+}
+config.window_padding = {
+    left = 10,
+    right = 25,
+    top = 5,
+    bottom = 0,
+}
 
--- Hyperlinks
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 
--- Native macOS fullscreen mode
-config.native_macos_fullscreen_mode = true
+-- -- Muxing by default
+-- config.unix_domains = {
+--   {
+--     name = 'unix',
+--   },
+-- }
+-- config.default_gui_startup_args = { 'connect', 'unix' }
+
 
 config.launch_menu = {
     {
@@ -31,35 +59,48 @@ config.launch_menu = {
         args = { '/bin/bash', '-l' },
     },
 }
--- config.font_size = 12
-config.font = wezterm.font {
-    family = 'MesloLGS NF',
-    weight = 'Regular'
-}
 
 
----@param arg { light: any, dark: any }
----@return any
-local function depending_on_appearance(arg)
-    local appearance = wezterm.gui.get_appearance()
-    if appearance:find 'Dark' then
-        return arg.dark
+-- Colorscheme depending on appearance
+-- ---@param arg { light: any, dark: any }
+-- ---@return any
+-- local function depending_on_appearance(arg)
+--     local appearance = wezterm.gui.get_appearance()
+--     if appearance:find 'Dark' then
+--         return arg.dark
+--     else
+--         return arg.light
+--     end
+-- end
+-- config.color_scheme = depending_on_appearance {
+--     -- light = 'Tokyo Night Day',
+--     light = 'Tokyo Night Storm',
+--     dark = 'Tokyo Night Storm',
+-- }
+
+local dark_theme = 'Tokyo Night Storm'
+local light_theme = 'Tokyo Night Day'
+
+-- Default color_scheme
+config.color_scheme = dark_theme
+
+-- Override color_scheme
+wezterm.on('toggle_color_scheme', function (window)
+    local overrides = window:get_config_overrides() or {}
+
+    if not overrides.color_scheme then
+        overrides.color_scheme = dark_theme
     else
-        return arg.light
+        overrides.color_scheme = overrides.color_scheme == light_theme and dark_theme or light_theme
     end
-end
-
--- config.color_scheme = 'Tokyo Night Storm'
-config.color_scheme = depending_on_appearance {
-    -- light = 'Tokyo Night Day',
-    light = 'Tokyo Night Storm',
-    dark = 'Tokyo Night Storm',
-}
+    window:set_config_overrides(overrides)
+end)
 
 -- Tab bar
+config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
-
--- Fancy tab bar appearance
+config.switch_to_last_active_tab_when_closing_tab = true
+config.tab_max_width = 32
 config.window_frame = {
     font = wezterm.font { family = "SF Pro Text", weight = "Medium" },
     font_size = 14,
@@ -67,19 +108,12 @@ config.window_frame = {
     inactive_titlebar_bg = '#F0F0F0',
 
 }
-
-config.window_padding = {
-    left = 10,
-    right = 10,
-    top = 5,
-    bottom = 0,
-}
-
 config.colors = {
   tab_bar = {
     -- The active tab is the one that has focus in the window
     active_tab = {
       bg_color = '#24283b',
+      -- bg_color = '#454545',
       fg_color = '#ffffff',
     },
 
@@ -111,9 +145,13 @@ config.colors = {
 
 -- Keys
 -- config.disable_default_key_bindings = true
-
-local act = wezterm.action
 config.keys = {
+    -- Toggle dark and light color_scheme
+    {
+        key = 'C',
+        mods = 'CTRL',
+        action = act.EmitEvent 'toggle_color_scheme',
+    },
 
     -- This will create a new split and run your default program inside it
     {
@@ -180,12 +218,10 @@ config.keys = {
 }
 
 
--- Dead keys for accented characters: left Option key performs composition
+-- Dead keys for accented characters:
+-- left Option key performs composition
 -- right Option does not. (As it is standard on the mac)
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = false
-
--- Do not resize window when change font size
-config.adjust_window_size_when_changing_font_size = false
 
 return config
