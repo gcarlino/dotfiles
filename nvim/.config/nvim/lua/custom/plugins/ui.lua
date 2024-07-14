daed3cffa11f80f75cf57491918a7afd4ef1ef69
Giuseppe Carlino <g.carlino@simularia.it>
2024-07-14 09:59:49 +0200

    Fix which-key for version 3.3 [nvim]

--- nvim/.config/nvim/lua/custom/plugins/ui.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui.lua
@@ -8,36 +8,22 @@ return {
     {
         "folke/which-key.nvim",
         event = "VeryLazy",
-        config = function ()
-            require('which-key').setup({
-                window = {
-                    border = 'single',
-                },
-            })
-
-            vim.o.timeout = true
-            vim.o.timeoutlen = 1000
-
-            -- Document existing key chains
-            require("which-key").register({
-                ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-                ["<leader>d"] = { name = "[D]AP", _ = "which_key_ignore" },
-                ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-                ["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
-                ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-                ["<leader>l"] = { name = "[L]SP", _ = "which_key_ignore" },
-                ["<leader>x"] = { name = "Trouble", _ = "which_key_ignore" },
-                ["<leader>v"] = { name = "Diff[V]iew", _ = "which_key_ignore" },
-                ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-                ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-                ["<leader>o"] = { name = "[O]bsidian", _ = "which_key_ignore" },
-            })
-            -- visual mode
-            require("which-key").register({
-                ["<leader>h"] = { "Git [H]unk" },
-            }, { mode = "v" })
-        end
-
+        opts = {
+            preset = "modern",
+            delay = 800,
+            spec = {
+                { "<leader>c", group = "[C]ode" },
+                { "<leader>d", group = "[D]AP" },
+                { "<leader>f", group = "[F]ind", icon = ""},
+                { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+                { "<leader>l", group = "[L]SP" },
+                { "<leader>o", group = "[O]bsidian" },
+                { "<leader>t", group = "[T]oggle" },
+                { "<leader>v", group = "Diff[V]iew + ..." },
+                { "<leader>w", group = "[W]orkspace" },
+                { "<leader>x", group = "Trouble" },
+            },
+        },
     },
 
     {
