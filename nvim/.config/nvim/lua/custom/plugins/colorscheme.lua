c6f8b08a593a54f73ea5825ecbec5e0eaeb35892
Giuseppe Carlino <g.carlino@simularia.it>
2024-07-22 12:03:27 +0200

    Switch to onedark colorscheme [nvim]
    to match default theme by ghostty.

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -3,33 +3,22 @@ return {
     {
         'navarasu/onedark.nvim',
         priority = 1000,
-        enabled = false,
         config = function()
-            vim.o.background='light'
             require('onedark').setup({
-                style = 'light',
+                style = 'dark',
             })
             require('onedark').load()
         end
     },
 
+    { "catppuccin/nvim",
+        name = "catppuccin",
+        event = "VeryLazy"
+    },
+
     {
         'folke/tokyonight.nvim',
-        lazy = false,
-        priority = 1000,
-        enabled = true,
-        config = function()
-            require('tokyonight').setup({
-                style = 'storm',
-                light_style = 'day',
-                styles = {
-                    floats = 'dark'
-                },
-                sidebars = { "qf", "help" },
-                lualine_bold = false,
-            })
-            vim.cmd.colorscheme('tokyonight-storm')
-        end
+        event = "VeryLazy"
     },
 
 }
