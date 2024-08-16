bad5546e70e0aa5208b9203938f538b9dbc1da2d
Giuseppe Carlino <g.carlino@simularia.it>
2024-08-16 22:59:05 +0200

    Cleanup git configuration [nvim]
    Remove unnecessary code and make configuration more similar to others.

--- nvim/.config/nvim/lua/custom/gitsigns.lua
+++ nvim/.config/nvim/lua/custom/gitsigns.lua
@@ -3,14 +3,14 @@ require('gitsigns').setup({
     on_attach = function(bufnr)
         local gitsigns = require('gitsigns')
 
-        local function map(mode, l, r, opts)
+        local function set(mode, l, r, opts)
             opts = opts or {}
             opts.buffer = bufnr
             vim.keymap.set(mode, l, r, opts)
         end
 
         -- Navigation
-        map('n', ']c', function()
+        set('n', ']c', function()
             if vim.wo.diff then
                 vim.cmd.normal({']c', bang = true})
             else
@@ -18,7 +18,7 @@ require('gitsigns').setup({
             end
         end)
 
-        map('n', '[c', function()
+        set('n', '[c', function()
             if vim.wo.diff then
                 vim.cmd.normal({'[c', bang = true})
             else
@@ -27,19 +27,19 @@ require('gitsigns').setup({
         end)
 
         -- Actions
-        map({ 'n', 'v' }, '<leader>hs', gitsigns.stage_hunk, { desc = "Git [s]tage hunk" })
-        map({ 'n', 'v' }, '<leader>hr', gitsigns.reset_hunk, { desc = "Git [r]eset hunk" })
-        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = "Gitsigns: [S]tage buffer" })
-        map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = "Gitsigns: [u]ndo stage hunk" })
-        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = "Gitsigs: [R]eset buffer" })
-        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = "Gitsigns: [p]review hunk" })
-        map('n', '<leader>hb', function() gitsigns.blame_line{ full = true } end, { desc = "Gitsigns: [b]lame line" })
-        map('n', '<leader>hd', gitsigns.diffthis, { desc = "Gitsigns: [d]iff this" })
-        map('n', '<leader>hD', function() gitsigns.diffthis('~') end, { desc = "Gitsigns: [D]iff against last commit" })
+        set({ 'n', 'v' }, '<leader>hs', gitsigns.stage_hunk, { desc = "Git [s]tage hunk" })
+        set({ 'n', 'v' }, '<leader>hr', gitsigns.reset_hunk, { desc = "Git [r]eset hunk" })
+        set('n', '<leader>hS', gitsigns.stage_buffer, { desc = "Git [S]tage buffer" })
+        set('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = "Git [u]ndo stage hunk" })
+        set('n', '<leader>hR', gitsigns.reset_buffer, { desc = "Git [R]eset buffer" })
+        set('n', '<leader>hp', gitsigns.preview_hunk, { desc = "Git [p]review hunk" })
+        set('n', '<leader>hb', function() gitsigns.blame_line{ full = true } end, { desc = "Git [b]lame line" })
+        set('n', '<leader>hd', gitsigns.diffthis, { desc = "Git [d]iff this" })
+        set('n', '<leader>hD', function() gitsigns.diffthis('~') end, { desc = "Git [D]iff against last commit" })
 
         -- Toggles
-        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = "Gitsigns: toggle current line blame" })
-        map('n', '<leader>td', gitsigns.toggle_deleted, { desc = "Gitsign: toggle deleted" })
+        set('n', '<leader>ht', gitsigns.toggle_current_line_blame, { desc = "Git toggle current line blame" })
+        set('n', '<leader>hl', gitsigns.toggle_deleted, { desc = "Git toggle deleted" })
 
     end
 })
--- nvim/.config/nvim/lua/custom/plugins/git.lua
+++ nvim/.config/nvim/lua/custom/plugins/git.lua
@@ -8,34 +8,11 @@ return {
 
     {
         "lewis6991/gitsigns.nvim",
-        event = { "BufReadPre", "BufNewFile" },
+        -- event = { "BufReadPre", "BufNewFile" },
+        event = "VeryLazy",
         config = function()
             require("custom.gitsigns")
         end
     },
 
-    {
-        'sindrets/diffview.nvim',
-        enabled = false,
-        event = "VeryLazy",
-        cmd = "DiffviewOpen",
-        keys = {
-            { '<leader>vo', ':DiffviewOpen<CR>', desc = "Open Diffview tab."},
-            { '<leader>vc', ':DiffviewClose<CR>', desc = "Close Diffview tab."},
-        },
-        config = function ()
-            require('diffview').setup({
-                enhanced_diff_hl = true,
-                view = {
-                    merge_tool = {
-                        layout = "diff3_mixed"
-                    }
-                },
-            })
-            vim.opt.fillchars = vim.opt.fillchars + 'diff:╱'
-        end,
-        dependencies = {
-            'nvim-lua/plenary.nvim'
-        }
-    }
 }
