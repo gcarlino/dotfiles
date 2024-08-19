7a2f901d3638682bb2cd2859c17251e54cdcc6cb
Giuseppe Carlino <g.carlino@simularia.it>
2024-08-19 22:18:32 +0200

    Do not lazy load default colorscheme [nvim]

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -3,6 +3,7 @@ return {
     {
         'navarasu/onedark.nvim',
         priority = 1000,
+        lazy = false,
         config = function()
             require('onedark').setup({
                 style = 'dark',
