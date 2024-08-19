22c23a59765be8209b63cbc1363f14b20d41e2c1
Giuseppe Carlino <g.carlino@simularia.it>
2024-08-19 19:44:54 +0200

    Fix R.nvim configuration ... [nvim]
    Add missing groups to which-key
    Fix output colourscheme

--- nvim/.config/nvim/lua/custom/plugins/R.lua
+++ nvim/.config/nvim/lua/custom/plugins/R.lua
@@ -14,12 +14,12 @@ return {
                             vim.api.nvim_buf_set_keymap(0, "n",
                                 "<LocalLeader>cl",
                                 "<Cmd>lua require('r.send').cmd('devtools::load_all()')<CR>",
-                                { desc = "R: devtools::load_all()", silent = true }
+                                { desc = "R devtools [c]ode [l]oad all", silent = true }
                             )
                             vim.api.nvim_buf_set_keymap(0, "n",
                                 "<LocalLeader>cd",
                                 "<Cmd>lua require('r.send').cmd('devtools::document()')<CR>",
-                                { desc = "R: devtools::document()", silent = true }
+                                { desc = "R devtools [c]ode [d]ocument", silent = true }
                             )
                         end
                     end,
@@ -48,8 +48,25 @@ return {
             vim.g.r_indent_align_args = 0
 
             -- Highlight R output using normal colourscheme
-            -- vim.g.rout_follow_colorscheme = true
+            vim.g.rout_follow_colorscheme = true
 
+            local wk = require('which-key')
+            wk.add({
+                buffer = true,
+                { '<LocalLeader>a', group = 'All' },
+                { '<LocalLeader>b', group = 'Between Marks' },
+                { "<localleader>c", group = "Chunks" },
+                { "<localleader>f", group = "Functions" },
+                { "<localleader>g", group = "Goto" },
+                { "<localleader>i", group = "Install" },
+                { "<localleader>k", group = "Knit" },
+                { "<localleader>p", group = "Paragraph" },
+                { "<localleader>q", group = "Quarto" },
+                { "<localleader>r", group = "R general" },
+                { "<localleader>s", group = "Split or send" },
+                { "<localleader>t", group = "Terminal" },
+                { "<localleader>v", group = "View" },
+            })
         end
     },
 
