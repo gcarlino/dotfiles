26106fb6dc9fdb997cc57f13559e0bc0e155711b
Giuseppe Carlino <g.carlino@simularia.it>
2024-08-22 23:58:50 +0200

    Add harpoon plugin [nvim]

---
+++ nvim/.config/nvim/lua/custom/plugins/harpoon.lua
@@ -0,0 +1,27 @@
+return {
+    "ThePrimeagen/harpoon",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    branch = "harpoon2",
+    event = "VeryLazy",
+    config = function()
+        local harpoon = require "harpoon"
+        harpoon:setup()
+
+        vim.keymap.set("n", "<localleader>ha", function()
+            harpoon:list():add()
+        end,
+            { desc = "[H]harpoon [a]dd" })
+        vim.keymap.set("n", "<localleader>hl", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+            { desc = "[H]arpoon [l]ist" }
+        )
+
+        -- Set <space>1..<space>5 be my shortcuts to moving to the files
+        for _, idx in ipairs { 1, 2, 3, 4, 5 } do
+            vim.keymap.set("n", string.format("<space>%d", idx), function()
+                harpoon:list():select(idx)
+            end)
+        end
+    end,
+}
