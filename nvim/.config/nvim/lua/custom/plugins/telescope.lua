025eab159e7b8322111f061a8bf1bf903dd8fe1a
Giuseppe Carlino <g.carlino@simularia.it>
2024-08-23 15:04:50 +0200

    Import fzf-lua plugin [nvim]

--- nvim/.config/nvim/lua/custom/plugins/telescope.lua
+++ nvim/.config/nvim/lua/custom/plugins/telescope.lua
@@ -1,4 +1,19 @@
 return {
+
+    {
+        "ibhagwan/fzf-lua",
+        cmd = "FzfLua",
+        keys = {
+            { "<C-space>", "<cmd>FzfLua buffers<CR>", desc = "Fzf-lua buffers" },
+            { "<C-p>", "<cmd>FzfLua files<CR>", desc = "Fzf-lua files" },
+            { "<C-/>", "<cmd>FzfLua helptags<CR>", desc = "Fzf-lua help" },
+            { "<C-\\>", "<cmd>FzfLua live_grep<CR>", desc = "Fzf-lua live_grep" },
+        },
+        -- optional for icon support
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {},
+    },
+
     {
         'nvim-telescope/telescope.nvim',
         event = "VeryLazy",
