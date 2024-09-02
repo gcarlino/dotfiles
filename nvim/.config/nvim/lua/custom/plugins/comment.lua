f78e079bba25a973666bc5afde79180fe83d1ff0
Giuseppe Carlino <g.carlino@simularia.it>
2024-09-02 21:27:22 +0200

    Update todo-comments configuration [nvim]
    Lazy load.
    Don't show signs in status column.

--- nvim/.config/nvim/lua/custom/plugins/comment.lua
+++ nvim/.config/nvim/lua/custom/plugins/comment.lua
@@ -1,7 +1,10 @@
 return {
     {
         "folke/todo-comments.nvim",
+        event = "VeryLazy",
         dependencies = { "nvim-lua/plenary.nvim" },
-        opts = {},
+        opts = {
+            signs = false
+        },
     }
 }
