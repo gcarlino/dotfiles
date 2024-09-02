e54bd16b5b2c6b6095c76a6b59e03e02c57cf621
Giuseppe Carlino <g.carlino@simularia.it>
2024-09-02 10:59:12 +0200

    Add buffer local keymaps key [nvim]
    in which-key. Also LazyLoad

--- nvim/.config/nvim/lua/custom/plugins/ui.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui.lua
@@ -2,7 +2,15 @@ return {
 
     {
         "folke/which-key.nvim",
-        event = "UIEnter",
+        event = "VeryLazy",
+        keys = {
+            { "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)"
+            },
+        },
         opts = {
             preset = "helix",
             delay = 800,
