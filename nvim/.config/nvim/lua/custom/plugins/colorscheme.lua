d800ae5e58b7cdd846622d5389c06378f81b19c6
Giuseppe Carlino <g.carlino@simularia.it>
2024-09-30 23:42:25 +0200

    Remove unused colorschemes [nvim]

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -4,20 +4,13 @@ return {
         'navarasu/onedark.nvim',
         priority = 1000,
         lazy = false,
+        cond = true,
         config = function()
             require('onedark').setup({
                 style = 'dark',
             })
             require('onedark').load()
         end
-    },
-
-    { "catppuccin/nvim",
-        name = "catppuccin",
-    },
-
-    {
-        'folke/tokyonight.nvim',
     },
 
 }
