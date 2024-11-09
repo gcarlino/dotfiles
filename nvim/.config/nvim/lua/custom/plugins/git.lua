f9ed2aca3d099c00e801f4992a7a6a9ff3a458fc
Giuseppe Carlino <g.carlino@simularia.it>
2024-11-10 00:45:03 +0100

    Add vim-flog plugin [nvim]

--- nvim/.config/nvim/lua/custom/plugins/git.lua
+++ nvim/.config/nvim/lua/custom/plugins/git.lua
@@ -6,6 +6,18 @@ return {
     },
 
     {
+        "rbong/vim-flog",
+        lazy = true,
+        cmd = { "Flog", "Flogsplit", "Floggit" },
+        dependencies = {
+            "tpope/vim-fugitive",
+        },
+        config = function ()
+            vim.g.flog_enable_extended_chars = true
+        end
+    },
+
+    {
         "lewis6991/gitsigns.nvim",
         event = "VeryLazy",
         opts = {
