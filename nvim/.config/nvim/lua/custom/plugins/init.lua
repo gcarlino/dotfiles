004a0dc2337e6f04bf9ca94005f3323736681670
Giuseppe Carlino <g.carlino@simularia.it>
2024-11-28 23:42:42 +0100

    Install oil plugin [nvim]

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -85,6 +85,16 @@ return {
     },
 
     {
+        'stevearc/oil.nvim',
+        cmd = { "Oil" },
+        opts = {},
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("oil").setup()
+        end
+    },
+
+    {
         "lervag/vimtex",
         lazy = false,
         ft = "tex",
