dd679dbc0e8008fdacffb55cb168de910e3d7e36
Giuseppe Carlino <g.carlino@simularia.it>
2024-12-01 20:19:44 +0100

    Reorder statuscol [nvim]

--- nvim/.config/nvim/lua/custom/plugins/ui.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui.lua
@@ -66,16 +66,16 @@ return {
                 setopt = true,
                 relculright = true,
                 segments = {
+                    {
+                        text = { builtin.foldfunc, " " },
+                        click = "v:lua.ScFa",
+                        -- hi = "NonText",
+                    },
                     { text = { "%s" }, click = "v:lua.ScSa", },
                     {
                         text = { builtin.lnumfunc, " " },
                         condition = { true, builtin.not_empty },
                         click = "v:lua.ScLa",
-                    },
-                    {
-                        text = { builtin.foldfunc, " " },
-                        click = "v:lua.ScFa",
-                        -- hi = "NonText",
                     },
                 },
             })
