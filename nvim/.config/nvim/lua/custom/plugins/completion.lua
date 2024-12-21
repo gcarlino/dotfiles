ef6953782455fc8b35d7b084c0b3e33a2e38feb8
Giuseppe Carlino <g.carlino@simularia.it>
2024-12-21 17:57:50 +0100

    Install blink.compat and sources [nvim]
    Additional installed sources:
    - cmp-r
    - cmp-latex-symbols
    - cmp-vimtex

--- nvim/.config/nvim/lua/custom/plugins/completion.lua
+++ nvim/.config/nvim/lua/custom/plugins/completion.lua
@@ -1,8 +1,22 @@
 -- Completion
 return {
+
+    {
+        'saghen/blink.compat',
+        enabled = true,
+        version = '*',
+        lazy = true,
+        opts = { enable_cmp_integration = true },
+    },
+
     {
         'saghen/blink.cmp',
-        dependencies = 'rafamadriz/friendly-snippets',
+        dependencies = {
+            'rafamadriz/friendly-snippets',
+            "R-nvim/cmp-r",
+            "micangl/cmp-vimtex",
+            'kdheepak/cmp-latex-symbols',
+        },
         version = '*',
         opts = {
             keymap = { preset = 'default' },
@@ -10,24 +24,44 @@ return {
                 use_nvim_cmp_as_default = true,
                 nerd_font_variant = 'mono'
             },
+            signature = {
+                enabled = true,
+                window = {
+                    border = 'single',
+                },
+            },
             completion = {
                 menu = {
-                    draw = {
-                        columns = {
-                            { "label", "label_description", gap = 1 },
-                            { "kind_icon", "kind", gap = 1 }
-                        },
-                    },
+                    border = 'single',
+                    -- draw = {
+                    --     columns = {
+                    --         { "label", "label_description", gap = 1 },
+                    --         { "kind_icon", "kind", gap = 1 },
+                    --         { "source_name" },
+                    --     },
+                    -- },
                 },
             },
             sources = {
-                default = { 'lsp', 'path', 'snippets', 'buffer' },
+                default = { 'lsp', 'path', 'snippets', 'buffer',
+                    'latex_symbols', 'cmp_r', 'lazydev', 'vimtex' },
+
                 providers = {
-                    lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+                    lazydev = {
+                        name = "LazyDev",
+                        module = "lazydev.integrations.blink",
+                        score_offset = 100,
+                    },
+                     cmp_r = {
+                        name = "cmp_r",
+                        module = "blink.compat.source",
+                        score_offset = 100,
+                    },
+                    latex_symbols = { name = "latex_symbols", module = "blink.compat.source", },
+                    vimtex = { name = 'vimtex', module = 'blink.compat.source' },
                 },
             },
         },
-        opts_extend = { "sources.default", "lazydev" }
     },
 
     {
