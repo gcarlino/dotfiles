1e536649d04709e3c392fccc9d8c74997771555f
Giuseppe Carlino <g.carlino@simularia.it>
2024-12-21 18:08:53 +0100

    Change colorscheme to catppuccin [nvim]

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,4 +1,14 @@
 return {
+
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function ()
+            vim.cmd.colorscheme("catppuccin")
+        end
+    },
+
     {
         "folke/tokyonight.nvim",
         lazy = false,
